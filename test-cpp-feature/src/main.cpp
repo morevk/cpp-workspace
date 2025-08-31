c62365ebd78f18140e8023f986cc10a3ed74a752
vishal.more <vishal.more@vertiv.com>
2025-08-31 21:55:55 +0530

    enabling test code in build

--- test-cpp-feature/src/main.cpp
+++ test-cpp-feature/src/main.cpp
@@ -5,7 +5,8 @@ int main()
     int test_lvalue_rvalue_refrence_main();
     int test_shared_ptr_main();
     int test_weak_ptr_main();
-    int test_exception_stack_unwind_nested();
+    int test_exception_stack_unwind_nested_main();
+    int test_exception_stack_unwind_main();
     int test_enum_main();
     int test_worker_main();
     int test_promise_future_main();
@@ -15,8 +16,12 @@ int main()
     int test_observer_pattern();
     int test_command_pattern();
     int test_lambda_main();
-
-
+    int test_template_variadic_log_main();
+    int test_template_argument_deduction_main();
+    int test_template_factory_method_main();
+    int test_template_for_each_main();
+    int test_inheritance_main();
+    
     // test_hello_main();
     // test_dynamic_array_main();
     // test_lvalue_rvalue_refrence_main();
@@ -29,8 +34,15 @@ int main()
     // test_singleton_pattern();
     // test_observer_pattern();
     // test_command_pattern();
-    // test_exception_stack_unwind_nested();
-    test_lambda_main();
+    // test_exception_stack_unwind_nested_main();
+    // test_exception_stack_unwind_main();
+    // test_lambda_main();
+    // test_template_variadic_log_main();
+    // test_template_argument_deduction_main();
+    // test_template_factory_method_main();
+    test_template_for_each_main();
+    // test_inheritance_main();
+
     return 0;
     
 }
